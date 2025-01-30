9f0d33b43dd857af44400600fa5274d4a1e177ef
Yuhang-Fan <fan.yuha@northeastern.edu>
2025-01-29 20:42:21 -0500

    Delete src/main/java/com/example/webapp/repository/HealthCheckRepository.java

--- src/main/java/com/example/webapp/repository/HealthCheckRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.webapp.repository;
-
-import com.example.webapp.model.HealthCheck;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HealthCheckRepository extends JpaRepository<HealthCheck, Long> {
-}
